7a013a7686c4bb9f00904417a54644e42e3b613f
yous6sef <110791674+yous6sef@users.noreply.github.com>
2026-05-20 23:59:21 +0300

    Fix start.sh - install npm dependencies before build

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-cd flix
+set -e
+
+cd /app/flix
+
+# Install dependencies
+npm install --omit=dev
+
+# Build webpack assets
+npm run build
+
+# Start PHP server
 php -S 0.0.0.0:${PORT:-8080}
